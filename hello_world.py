ec046d11a19db198d6e4c9bb7fb83392c73485b0
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-26 13:52:43 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+
+# Problem: Print Hello, World!
+# Platform: HackerRank
+# Difficulty: Easy
+
+# Description:Print the string "Hello, World!" to the console.
+
+# Approach:
+#- Store the string in a variable
+#- Print the variable using print()
+
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+
+#Code
+
+my_string = "Hello, World!"
+print(my_string)

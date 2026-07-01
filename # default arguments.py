cfadddfdeaed546a2d4bbc16bacf145ca83f45e9
Janvee Sharma <jsharma22072004@gmail.com>
2026-07-01 22:12:07 +0530

    Add files via upload

---
+++ # default arguments.py
@@ -0,0 +1,35 @@
+# ============================
+# PLATFORM:
+# HackerRank
+# PROBLEM:
+# Default Arguments
+# ============================
+
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+def print_from_stream(n, stream=EvenStream()):
+
+    # Reset the stream
+    stream.__init__()
+
+    # Print first n values
+    for _ in range(n):
+        print(stream.get_next())
